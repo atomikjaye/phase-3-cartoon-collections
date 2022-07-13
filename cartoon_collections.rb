4ad8b423d6d981bf7fadc115fc6354ead8de6ca8
Jocelyn Naarden <tzionone@gmail.com>
2022-07-13 12:31:57 -0400

    Finished Assignment

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,26 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves array# code an argument here
   # Your code here
+  array.map.with_index { |dwarf, index| puts "#{index + 1}. #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
+  planeteer_calls.map{ |call| call.capitalize + "!" }
+
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(long_calls)# code an argument here
   # Your code here
+  long_calls.find.any?{ |call| call.length > 4}
+
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(foods)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  # take foods array
+  cheese_types.find do |cheese|
+    foods.include?(cheese)
+  end
+  # check if array contains anything in cheese_types arry (loop)
 end
